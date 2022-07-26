8fea70cd17ecc92f01ef91b49994aeeff137f04b
Javad Rahimi <javad321javad@gmail.com>
2022-07-26 12:12:45 +0430

    feat: add function decorator

--- Decorator/decorator.cpp
+++ Decorator/decorator.cpp
@@ -30,14 +30,39 @@ template <typename T> string TransparentShape<T>::str() const
         << trans_degree;
         return oss.str();
 }
+void printer()
+{
+    cout<<"I'm a printer"<<endl;
+}
+
+int add(int a, int b)
+{
+    cout<<"Doing calculation"<<endl;
+
+    return a+b;
+}
 int main(void)
 {
-    Circle circle{0.5f};
-    ColoredShape cs{circle,"red"};
-    TransparentShape<Circle> tshape{0.1f};
-    cout<<cs.shape.str()<<std::endl;
-    cout <<tshape.str()<<std::endl;
+//    Circle circle{0.5f};
+//    ColoredShape cs{circle,"red"};
+//    TransparentShape<Circle> tshape{0.1f};
+//    cout<<cs.shape.str()<<std::endl;
+//    cout <<tshape.str()<<std::endl;
+    Logger c(printer, "Hello Function");
 
+    c();
+    auto call = make_logger2(
+                []()
+    {
+        cout<<"Hello 2"<<endl;
+    }, "Hello2 Function");
+    call();
 
+//    auto call2 = make_logger3(add,"Add");
+//    auto result = call2(2,5);
+    auto caller = Logger3<int(int,int)>(add, "test");
+       auto res = caller(3, 4);
+       std::cout << "result: " << res << std::endl;
+       return 0;
     return 0;
 }
--- Decorator/decorator.h
+++ Decorator/decorator.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <functional>
 using namespace std;
 
 class Decorator
@@ -70,5 +71,110 @@ template <typename T> struct TransparentShape: T
         trans_degree{trans}{}
 };
 
+struct Logger
+{
+    std::function<void()> func;
+    string name;
 
+    Logger(const function<void()>& func,
+           const string& name):
+        func{func},
+        name{name}
+    {
+
+    }
+
+    void operator()() const
+    {
+        cout<< "Entering "<<name<<endl;
+        func();
+        cout<<"Exiting "<<name<<endl;
+    }
+    void operator()(int i)const
+    {
+        cout<<"Works: "<<i<<endl;
+    }
+
+};
+
+template <typename Func>
+struct Logger2
+{
+    Func func;
+    string name;
+
+    Logger2(const Func& func,
+            const string& name):
+        func{func},
+        name{name}
+    {
+
+    }
+    void operator()() const
+    {
+        cout<< "Entering "<<name<<endl;
+        func();
+        cout<<"Exiting "<<name<<endl;
+    }
+};
+
+template <typename Func>
+auto make_logger2(
+        Func func,
+        const string& name)
+{
+    return Logger2<Func>{func, name};
+};
+
+template <typename F>
+class Logger3;
+template <typename R, typename... Args>
+class Logger3<R(Args...)>
+{
+    public:
+    Logger3(function<R(Args...)> func,
+            const string& name):
+        func{func},
+        name{name}
+    {}
+    R operator() (Args ...args)
+    {
+        cout << "Entering " << name << endl;
+        R result = func(std::forward<Args>(args)...);
+        std::cout << "Exiting " << name << std::endl;
+        return result;
+    }
+    private:
+    function<R(Args...)> func;
+    string name;
+};
+template <typename R, typename... Args>
+class Logger4
+{
+public:
+    Logger4(std::function<R(Args...)> func,
+        const std::string& name) :
+        func{ func },
+        name{ name }
+    {}
+    R operator() (Args ...args)
+    {
+        std::cout << "Entering " << name << std::endl;
+        R result = func(std::forward<Args>(args)...);
+        std::cout << "Exiting " << name << std::endl;
+        return result;
+    }
+private:
+    std::function<R(Args...)> func;
+    std::string name;
+};
+
+template <typename R, typename... Args>
+auto make_logger3(R (*func)(Args...),
+                  const string& name)
+{
+    return Logger3<R(Args...)>(
+                std::function<R(Args...)>(func),
+                name);
+}
 #endif // DECORATOR_H
