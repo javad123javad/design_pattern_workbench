f1e72421806b8c6a19dbfaba42bf2cf7c0d9db17
Javad Rahimi <javad321javad@gmail.com>
2022-07-29 10:53:03 +0430

    feat: add virtual proxy

--- Proxy/proxy.h
+++ Proxy/proxy.h
@@ -2,7 +2,7 @@
 #define PROXY_H
 
 #include <ostream>
-
+#include <iostream>
 class Proxy
 {
 public:
@@ -33,6 +33,59 @@ protected:
 };
 /////////// Virtual Proxy
 
+class Image
+{
+public:
+    virtual void draw() = 0;
+};
+
+class Bitmap: Image
+{
+public:
+    Bitmap(const std::string& file_name):
+        file_name{file_name}
+    {
+        std::cout<< "Loading "<<file_name<<
+                    " From file.\n";
+    }
+
+    void draw() override
+    {
+        std::cout<<"Drawing the "<< file_name<<std::endl;
+    }
+
+private:
+    std::string file_name;
+};
+
+class  LazyBitmap: Image
+{
+    LazyBitmap(const std::string& file_name):
+        file_name{file_name}{}
+    ~LazyBitmap(){delete bmp;}
+
+    void draw() override
+    {
+        if(!bmp)
+        {
+            bmp = new Bitmap(file_name);
+            bmp->draw();
+        }
+
+    }
+
+private:
+    Bitmap *bmp{nullptr};
+    std::string file_name;
+
+};
+
+void draw_image(Image& img)
+{
+    std::cout<<"Gonna to draw the image"<<std::endl;
+    img.draw();
+
+}
 #endif // PROXY_H
 
 
